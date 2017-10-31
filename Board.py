313ebf7ac19ae875e4c75d1952491e3573bb5a20
stefanpeidli <stefanpeidli@gmail.com>
2017-10-31 15:09:07 +0100

    Test Stefan 1
    deine mudda

--- Board.py
+++ Board.py
@@ -42,7 +42,8 @@ class Board:
                     elif self.vertices[adj_x, adj_y] == self.vertices[start_x, start_y]:
                         self.group.append((adj_x, adj_y))
                         
-                        
+        if 1>0:
+            print("deinemudda");
                     
         if capture:
             for x,y in self.group:
