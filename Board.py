507408237e17fc3b3d717cdff912c49633f6c62a
paddyooo <paddyooo@users.noreply.github.com>
2017-10-31 15:08:33 +0100

    sd
    asdf

--- Board.py
+++ Board.py
@@ -37,12 +37,15 @@ class Board:
             for dx,dy in dxdys:
                 adj_x, adj_y = x + dx, y + dy
                 if self.is_on_board(adj_x,adj_y) and not self.group.__contains__((adj_x, adj_y)):
+                    if(self.group.__contains__((adj_x, adj_y))):
+                        print("true")
                     if self.vertices[adj_x, adj_y] == Stone.Empty:
                         return True
                     elif self.vertices[adj_x, adj_y] == self.vertices[start_x, start_y]:
                         self.group.append((adj_x, adj_y))
                         
-                        
+        
+                
                     
         if capture:
             for x,y in self.group:
