9e52dbd654953ce5f34220dcfd6f489e4813b22a
Stefan Peidli <stefanpeidli@gmail.com>
2017-12-07 10:17:02 +0100

    Training for Networks

--- TrainingScript.py
+++ TrainingScript.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Hashable import Hashable
 from TrainingDataFromSgf import TrainingDataSgf
-import PolicyNet
+from PolicyNet import PolicyNet
 import time
 import datetime
 
@@ -25,6 +25,18 @@ Parameters:
     tolerance : Error tolerance. Training will stop if error falls below it. [up to now, the error of 4 will not be reached that fast..., take what you like here between 0 and 3]
     maxepochs : Maximal amount of epochs to iterate. Training will stop once this epoch is reached. [1, 30, inf] keep the computation time in mind! in case, better run many small Trainings so the results are not lost!
 
+On saving and loading weights:
+    FYI, weights can be loaded and saved into a folder < Saved_Weights > which is already in git.ignore. Please create this folder if it does not exist yet.
+    Let's say your trained your PolicyNet called Martha, then:
+        Saving weights: Martha.saveweights('Saved_Weights', name)
+        Loading weights: Martha.loadweightsfromfile('Saved_Weights',name)
+    I also added a proposal for a weight naming format:
+        name="weights"+datetime.datetime.now().strftime("%y%m%d%H%M")+"eta10000"+str(int(eta*10000))+"epochs"+str(epochs)+"batchsize"+"1"+"Dan10"
+    I think this format is rather self-explaining. E.g.
+        weights1712061818eta10000100epochs20batchsize1Dan10
+        weights from 2017-12-06 at 18:18 with eta*10 000 = 100, epochs trained = 20, batchsize = 1, Dataset was Dan_10
+        
+        
 """
 
 def TrainingBasic(PolicyNetwork,learningrate,epochs,sgfrange):
@@ -34,7 +46,7 @@ def TrainingBasic(PolicyNetwork,learningrate,epochs,sgfrange):
     for i in range(0,epochs):
         [f,o,error] = PolicyNetwork.Learnpropagate(eta ,testdata)
         errors_by_epoch.append(error)
-    return errors_by_epoch
+    #return errors_by_epoch
 
     
 def TrainingSplit(PolicyNetwork, learningrate, maxepochs, sgfrange, trainingrate, tolerance):
@@ -70,7 +82,7 @@ def TrainingBasicDan10(PolicyNetwork,learningrate,epochs):
 
    
 def ComparisonTraining1(PolicyNetwork,learningrate,epochs,batchsize):
-    w=TestNet.weights
+    w=PolicyNetwork.weights
     testset = TrainingDataSgf("dgs","dan_data_10")
     t1=time.time()
     errorinit = PolicyNetwork.PropagateSet(testset)
@@ -109,4 +121,21 @@ def ComparisonTraining1(PolicyNetwork,learningrate,epochs,batchsize):
     #plt.plot(range(epochs),errors2,'r')
     
 
+# Training Area = The Neural Network Gym : Do training here
+    
+your_name="Example"
 
+# example for training:
+if your_name is "Example":
+    MyNetwork = PolicyNet()
+    learningrate = 0.01
+    epochs = 5 #one epoch ~ 25 seconds
+    sgfrange = 10
+    TrainingBasic(MyNetwork , learningrate, epochs, sgfrange)
+    name="weights"+datetime.datetime.now().strftime("%y%m%d%H%M")+"eta10000"+str(int(learningrate*10000))+"epochs"+str(epochs)+"batchsize"+"1"+"sgfrange"+str(sgfrange)
+    MyNetwork.saveweights('Saved_Weights', name)
+
+# Stefan:
+if your_name is "Stefan":
+    #hier schreibe ich mein training rein
+    print("halo I bims")
